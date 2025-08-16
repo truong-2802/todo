b4aa3309acb759d63316d49df5d2c6600a1377f2
truong-2802 <115643911+truong-2802@users.noreply.github.com>
2025-08-16 22:53:08 +0700

    b23

--- main.js
+++ main.js
@@ -37,21 +37,69 @@ function renderTasks() {
         taskList.innerHTML = `<li>Danh sach trong</li>`
         return;
     }
-  const html = tasks.map((task) => {
-    return `
-        <li class="task-item">
-            <span class="task-title">${task.name}</span>
-            <div class="task-action">
-                <button class="task-btn edit">Edit</button>
-                <button class="task-btn done">Mark as done</button>
-                <button class="task-btn delete">Delete</button>
-            </div>
-        </li>
-        `
-  }).join("");
+    // c2
+    taskList.innerHTML = ""
+
+    tasks.forEach((task)=>{
+      const li = document.createElement("li");
+      li.className = "task-item"
+      taskList.appendChild(li)
+
+      const span = document.createElement("span");
+      span.className = "task-title"
+      span.textContent = task.name;
+      li.appendChild(span)
+
+      const div = document.createElement("div");
+      div.className = "task-action"
+      li.appendChild(div)
+
+      const button = document.createElement("button")
+      button.className = "task-btn edit"
+      button.innerText = "Edit"
+      div.appendChild(button)
+
+      const button1 = document.createElement("button")
+      button1.className = "task-btn done"
+      button1.innerText= "Mark as done"
+      div.appendChild(button1)
+
+      const button2 = document.createElement("button")
+      button2.className = "task-btn delete"
+      button2.innerText= "Delete"
+      div.appendChild(button2)
+
+    })
+
+    // c1
+  // const html = tasks.map((task) => {
+  //   return `
+  //       <li class="task-item">
+  //           <span class="task-title">${escapeHTML(task.name)}</span>
+  //           <div class="task-action">
+  //               <button class="task-btn edit">Edit</button>
+  //               <button class="task-btn done">Mark as done</button>
+  //               <button class="task-btn delete">Delete</button>
+  //           </div>
+  //       </li>
+  //       `
+  // }).join("");
 
 
-  taskList.innerHTML = html;
+  // taskList.innerHTML = html;
 }
+
+// c1
+// function escapeHTML(str){
+//   const escape = document.querySelector("#escape")
+//   escape.innerText = str;
+//   const result = escape.innerHTML;
+
+//   escape.innerHTML = "";
+
+//   return result;
+
+// }
+
 
 renderTasks();
